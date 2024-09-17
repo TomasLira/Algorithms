d653b5d7b837f2cef79af01fbaf4863fce5f44c2
TomasLira <tomaspaivadelira1@gmail.com>
2024-09-17 13:51:42 -0300

    remove dp questions

--- Problems/solutions/01.py
+++ Problems/solutions/01.py
@@ -1,24 +1,26 @@
 # Solution 1 O(n) Time
-def top_k_frequent(nums,k):
+def top_k_frequent(nums, k):
     hash_table = {}
-    # Get frequencies for each term in list 
     for num in nums:
-        hash_table[num] = hash_table.get(num,0) + 1
+        hash_table[num] = hash_table.get(num, 0) + 1
     
-    # Index of bucket associated with frequency
-    buckets = [[] for _ in range(len(nums)+1)]
+    buckets = [[] for _ in range(len(nums) + 1)]
     
-    for num,freq in hash_table.items():
+    for num, freq in hash_table.items():
         buckets[freq].append(num)
+    
     result = []
 
-    # get biggest frequency until k
-    for bucket_idx in range(len(buckets)-1,-1,-1):
+    for bucket_idx in range(len(buckets) - 1, -1, -1):
         for num in buckets[bucket_idx]:
             result.append(num)
-        if len(result) == k:
-            break
+            if len(result) == k:
+                return result
     return result
+
+
+print(top_k_frequent([1,1,1,2,3,4,4,5],2))
+
 
 
 # Solution 2 O(n log k) Time
@@ -36,3 +38,4 @@ def top_k_frequent(nums,k):
         if len(min_heap) > k:
             heapq.heappop(min_heap)
     return [num for freq,num in min_heap] 
+
