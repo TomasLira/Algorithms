a5053355a8b57797ce54dfbf4db797cae16eaa63
TomasLira <tomaspaivadelira1@gmail.com>
2024-09-17 15:15:32 -0300

    add q20

--- Problems/solutions/20.py
+++ Problems/solutions/20.py
@@ -1,2 +1,23 @@
+# Solution O(n^2) using dp
+import bisect
 def longest_sub(nums):
-    ...
+    dp = [1] * len(nums)
+    max_length = 0
+    for end in range(len(nums)):
+        for start in range(end):
+            if nums[end] > nums[start]:
+                dp[end] = max(dp[end], dp[start] + 1)
+        max_length = max(max_length, dp[end])
+    return max_length
+
+# Solution O(nlog(n)
+def longest_sub(nums):
+    dp = []
+    for num in nums:
+        # Use bs to find correct sorted pos in dp list
+        pos = bisect.bisect_left(dp,num)
+        if pos == len(dp):
+            dp.append(num)
+        else:
+            dp[pos] = num
+    return len(dp)
