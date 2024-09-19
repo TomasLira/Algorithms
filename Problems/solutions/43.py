c1aaba4ffa3cb25540c83f9b4a0d6c2b5c6f552a
TomasLira <tomaspaivadelira1@gmail.com>
2024-09-18 22:19:22 -0300

    added q

--- Problems/solutions/43.py
+++ Problems/solutions/43.py
@@ -1,43 +1,23 @@
 from sys import maxsize
-
+# Solution O(n) Time
 def maximum_prod(nums):
-    max1 = max2 = -maxsize  
-    min1 = min2 = maxsize  
-
-    for num in nums:
-        if num >= 0:
+        max1 = max2 = max3 = -maxsize
+        min1 = min2 = maxsize
+        
+        for num in nums:
             if num > max1:
-                max2 = max1
-                max1 = num
+                max1, max2, max3 = num, max1, max2
             elif num > max2:
-                max2 = num
-        else:
+                max2, max3 = num, max2
+            elif num > max3:
+                max3 = num
+
             if num < min1:
-                min2 = min1
-                min1 = num
+                min1, min2 = num, min1
             elif num < min2:
                 min2 = num
-
-    max_pos_pair = max1 * max2
-    max_neg_pair = min1 * min2
-    return max((max1,max2),(min1,min2))
-
-print(maximum_prod([-100,-98,-1,2,3,4]))
-
+        return max(max2 * max3 * max1, min1 * min2 * max1)
         
-    
-        
-        
-        
-        
-    
-        
-
-print(maximum_prod([-100,-98,-1,2,3,4]))
-
-            
-    
-
 # If subarray had to be contiguous
 def maximum_prod(nums):            
     current_prod,maximum_prod = 1,-maxsize
